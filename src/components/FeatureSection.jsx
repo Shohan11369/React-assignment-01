0fe5094f5aca4a76a7d473408dcb017c8959b711
Shohan111369 <sarkershohan346@gmail.com>
2025-08-01 21:42:46 +0800

    Add maping icon,title,and desc

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -6,6 +6,23 @@ import ScatterPlotIcon from "@mui/icons-material/ScatterPlot";
 import OpenWithIcon from "@mui/icons-material/OpenWith";
 
 function FeaturesSection() {
+  const features = [
+    {
+      icon: <AutoFixHighIcon fontSize="large" />,
+      title: "You're in Control",
+      desc: "aiDRs operate based on your rules. You guide the machine, the aiDRs execute the work.",
+    },
+    {
+      icon: <ScatterPlotIcon fontSize="large" />,
+      title: "Infinitely Scalable",
+      desc: "Train an aiDR on a patch. When you are happy with the results, scale it effortlessly.",
+    },
+    {
+      icon: <OpenWithIcon fontSize="large" />,
+      title: "Incredibly Flexible",
+      desc: "Train an aiDR on an outbound patch, to nurture inbound leads, or to reach out to your long tail of SMB customers at renewal time.",
+    },
+  ];
   return (
     <div className="bg-[#04232e] text-white py-16 px-6">
       <div className="max-w-7xl mx-auto">
