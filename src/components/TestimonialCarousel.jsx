89eac75200889b4b5b06d334cbf6ef9c3bea93dd
Shohan111369 <sarkershohan346@gmail.com>
2025-08-04 16:57:36 +0800

    Added usestate,useefect,next

--- src/components/TestimonialCarousel.jsx
+++ src/components/TestimonialCarousel.jsx
@@ -5,6 +5,44 @@ import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 function TestimonialCarousel() {
+  const testimonials = [
+    {
+      quote: "He is Big Boss in this company",
+      name: "John Doe",
+      title: "Chief Strategy Officer @ Company",
+    },
+    {
+      quote: "He is HR this Company,Very intelligence person",
+      name: "Jane Smith",
+      title: "Product Lead @ Startup",
+    },
+    {
+      quote: "He is the director of this company",
+      name: "Ali Rahman",
+      title: "UX Designer @ Agency",
+    },
+  ];
+
+  const [current, setCurrent] = useState(0);
+  const length = testimonials.length;
+  const autoRef = useRef();
+
+  useEffect(() => {
+    clearTimeout(autoRef.current);
+    autoRef.current = setTimeout(() => {
+      setCurrent((prev) => (prev + 1) % length);
+    }, 6000);
+    return () => clearTimeout(autoRef.current);
+  }, [current, length]);
+
+  const prev = () => {
+    clearTimeout(autoRef.current);
+    setCurrent((prev) => (prev - 1 + length) % length);
+  };
+  const next = () => {
+    clearTimeout(autoRef.current);
+    setCurrent((prev) => (prev + 1) % length);
+  };
   return (
     <>
       <div>
